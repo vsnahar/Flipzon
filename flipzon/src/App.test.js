14664185b7ec4b50ae8c0811e62a4695ecbcaa70
Vaibhav Singh Nahar <vsnahar@gmail.com>
2023-06-22 16:14:57 +0530

    cloud test

--- flipzon/src/App.test.js
+++ flipzon/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Flipzon/i);
+  const linkElement = screen.getByText(/cloud test/i);
   expect(linkElement).toBeInTheDocument();
 });
