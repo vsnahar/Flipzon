acde2cdea957e376ead191699e6c05bd46c510ac
vsnahar <vsnahar@gmail.com>
2023-06-29 16:00:24 +0530

    Update App.js

--- flipzon/src/App.js
+++ flipzon/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Vaibhav Singh Nahar --- cloud test 5</h1>
+     <h1>Vaibhav Singh Nahar --- cloud test 6</h1>
     </div>
   );
 }
